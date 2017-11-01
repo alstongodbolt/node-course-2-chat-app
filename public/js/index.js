6b6aefb874c9fe888509987c8a1f814c479703cb
Alston Godbolt <godbolta@onid.oregonstate.edu>
2017-11-01 09:43:19 -0500

    Emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,7 @@ var socket = io()
 socket.on('connect', function () {
   console.log('Connected to server')
 
-  socket.emit('createMessage', {
-    from: 'Alston',
-    text: 'That works for me'
-  })
+  
 })
 
 socket.on('disconnect', function () {
