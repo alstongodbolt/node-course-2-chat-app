d65f36069ea85b6661ab0a30a0770e324f846e7a
Alston Godbolt <godbolta@onid.oregonstate.edu>
2017-11-02 10:47:34 -0500

    Add gelolcation support via api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect')
-var {generateMessage} = require('./message')
+var {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generate<essage', () => {
   it('should generate correct message object', () => {
@@ -14,3 +14,17 @@ describe('generate<essage', () => {
     })
   })
 })
+
+  describe('generateLocationMessage', () => {
+      it('should generate correct location object', () => {
+        var from = 'Deb'
+        var latitude = 15
+        var longitude = 19
+        var url = 'https://www.google.com/maps?q=15,19'
+        var message = generateLocationMessage(from, latitude, longitude)
+
+        expect(message.createdAt).toBeA('number')
+        expect(message).toInclude({from, url})
+      })
+  })
+
